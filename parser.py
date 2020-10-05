d00ccfdf72f4a53c97dfb10bd43b6a2b4bd601bf
MatusPilnan <m.pilnan@gmail.com>
2020-10-05 10:48:58 +0200

    remove pandas, split to multiple files

--- parser.py
+++ parser.py
@@ -1,9 +1,19 @@
 import glob
-from pprint import pprint
-import re
-from parse import *
-from parse import compile, findall
-import pandas as pd
+import os
+
+from parse import compile
+
+
+def close_files(d: dict):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            close_files(v)
+        else:
+            try:
+                print(f'Closing file {f.name}')
+                v.close()
+            except:
+                print(f'Cant close file: {v}')
 
 
 def get_filenames(path='', extension='.mp3'):
@@ -20,44 +30,53 @@ def get_filenames(path='', extension='.mp3'):
 
 path = 'wikipedia_dumps\\'
 
-filename_parser = compile(path+'{lang}wiki-latest-{tablename}.sql')
+filename_parser = compile(path + '{lang}wiki-latest-{tablename}.sql')
 page_parser = compile("({page_id:d},{namespace:d},'{page_title}',{rest})")
 langlink_parser = compile("({page_id:d},'{target_lang}','{target_title}')")
 
 files = get_filenames(path, '.sql')
 # files = ['wikipedia_dumps\\skwiki-latest-page.sql']
-pages = dict()
-langlinks = dict()
+page_files = dict()
+langlink_files = dict()
 
 for file in files:
     print(f'parsing {file}...')
     filename_result = filename_parser.search(file)
     lang = filename_result['lang']
-    if filename_result['tablename'] == 'page':
+    tablename = filename_result['tablename']
+    if tablename == 'page':
         parser = page_parser
-        collection = pages
     else:
         parser = langlink_parser
-        collection = langlinks
     with open(file, 'r', encoding='utf8', errors='ignore') as f:
         for line in f:
             if line.startswith('INSERT INTO'):
                 start = line.find('(')
                 line = line[start:]
                 result = parser.findall(line)
+                if tablename == 'page':
+                    try:
+                        target_file = page_files[lang]
+                    except KeyError:
+                        os.makedirs(f'csv/{tablename}', exist_ok=True)
+                        target_file = open(f'csv/{tablename}/{lang}.csv', 'w', encoding='utf8')
+                        page_files[lang] = target_file
 
-                if filename_result['tablename'] == 'page':
-                    new_entries = pd.DataFrame([(r['page_id'], r['page_title']) for r in result],
-                                 columns=['id', 'title'])
-                else:
-                    new_entries = pd.DataFrame([(r['page_id'], r['target_lang'], r['target_title']) for r in result],
-                                               columns=['id', 'target_lang', 'target_title'])
-                try:
-                    if collection[lang] is not None:
-                        collection[lang].append(new_entries)
-                    else:
-                        collection[lang] = new_entries
-                except KeyError:
-                    collection[lang] = new_entries
-        collection[lang].to_csv(f'csv/{filename_result["tablename"]}/{lang}.csv')
-    pass
+                for r in result:
+                    try:
+                        target_file.write(f'{r["page_id"]},{r["page_title"]}\n')
+                    except (KeyError, NameError):
+                        try:
+                            target_file = langlink_files[lang][r['target_lang']]
+                        except KeyError:
+                            os.makedirs(f'csv/{tablename}/{lang}', exist_ok=True)
+                            target_file = open(f'csv/{tablename}/{lang}/to_{r["target_lang"]}.csv', 'w',
+                                               encoding='utf8')
+                            try:
+                                langlink_files[lang][r['target_lang']] = target_file
+                            except KeyError:
+                                langlink_files[lang] = dict()
+                                langlink_files[lang][r['target_lang']] = target_file
+                        target_file.write(f'{r["page_id"]},{r["target_title"]}\n')
+    close_files(page_files)
+    close_files(langlink_files)
